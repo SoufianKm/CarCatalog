ede51f81c8f92e444601c7b02a2d5324d162e392
soufiankamma <soufian.kamma@gmail.com>
2025-02-06 00:05:28 +0100

    Create View State for Car Catalog UI screen

---
+++ app/src/main/java/com/soufianekamma/carcatalog/home/presentation/car_screen/CarViewState.kt
@@ -0,0 +1,8 @@
+package com.soufianekamma.carcatalog.home.presentation.car_screen
+
+import com.soufianekamma.carcatalog.home.domain.model.Car
+
+data class CarViewState(
+    val cars: List<Car> = emptyList(),
+    val error: String? = null
+)
