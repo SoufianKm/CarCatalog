96520215f9bc0624656557cf96ef03be9f658bd1
soufiankamma <soufian.kamma@gmail.com>
2025-02-06 00:04:19 +0100

    Create Car Catalog UI screen

---
+++ app/src/main/java/com/soufianekamma/carcatalog/home/presentation/car_screen/CarScreen.kt
@@ -0,0 +1,41 @@
+package com.soufianekamma.carcatalog.home.presentation.car_screen
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.soufianekamma.carcatalog.home.presentation.car_screen.components.CarCard
+
+@Composable
+fun CarScreen(modifier: Modifier = Modifier) {
+
+}
+
+
+@Composable
+fun CarsList(state: CarViewState) {
+    Scaffold(
+        modifier = Modifier.fillMaxSize()
+    ) { paddingValues ->
+
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceBetween,
+            contentPadding = PaddingValues(all = 8.dp)
+        ) {
+            items(state.cars) { car ->
+                CarCard(car = car)
+            }
+        }
+    }
+}
