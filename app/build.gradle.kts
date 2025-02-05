afb48d4941769b2db162d849f24889a5eff7b53e
soufiankamma <soufian.kamma@gmail.com>
2025-02-05 22:44:48 +0100

    Add Car data class

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,6 @@ android {
     defaultConfig {
         applicationId = "com.soufianekamma.carcatalog"
         minSdk = 26
-        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
---
+++ app/src/main/java/com/soufianekamma/carcatalog/home/domain/model/Car.kt
@@ -0,0 +1,8 @@
+package com.soufianekamma.carcatalog.home.domain.model
+
+data class Car(
+    val id: Int,
+    val title: String,
+    val description: String,
+    val imageUrl: String
+)
