175df7d4a37a56b51a2b0fcea57774b96a7f0b18
soufiankamma <soufian.kamma@gmail.com>
2025-02-06 14:21:27 +0100

    Create application class

--- app/src/main/java/com/soufianekamma/carcatalog/MainActivity.kt
+++ app/src/main/java/com/soufianekamma/carcatalog/MainActivity.kt
@@ -11,37 +11,19 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.soufianekamma.carcatalog.home.presentation.car_screen.CarScreen
 import com.soufianekamma.carcatalog.ui.theme.CarCatalogTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             CarCatalogTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                CarScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CarCatalogTheme {
-        Greeting("Android")
     }
 }
